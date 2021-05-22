53e3a252575b33b443160a68a27dc4ee44963ad1
Vazzi <45903049+VazziDE@users.noreply.github.com>
2021-05-22 17:24:59 +0200

    Added Config Use

--- ProxyBroadcast/src/main/java/de/vazzi/BroadcastCommand.java
+++ ProxyBroadcast/src/main/java/de/vazzi/BroadcastCommand.java
@@ -11,9 +11,9 @@ public class BroadcastCommand extends Command {
 
     public BroadcastCommand() {
         super("broadcast", CommandSettings.builder()
-                .setDescription("Broadcast a message to all players")
-                .setPermission("broadcast.command")
-                .setUsageMessage("/broadcast Message")
+                .setDescription(ProxyBroadcast.cfg.getString("cmd-description"))
+                .setPermission(ProxyBroadcast.cfg.getString("cmd-permission"))
+                .setUsageMessage(ProxyBroadcast.cfg.getString("cmd-usage"))
                 .build());
     }
 
@@ -26,11 +26,11 @@ public class BroadcastCommand extends Command {
         if(strings.length < 1){
             return false;
         }
-        String prefix = "§bBroadcast §f» ";
+        String prefix = ProxyBroadcast.cfg.getString("prefix");
         for (ProxiedPlayer player: ProxyServer.getInstance().getPlayers().values()) {
             player.sendMessage(prefix + String.join(" ", strings));
         }
-        commandSender.sendMessage(prefix + "§aThe message has been sent to all players.");
+        commandSender.sendMessage( prefix + ProxyBroadcast.cfg.getString("send-succes"));
         return true;
     }
 }
--- ProxyBroadcast/src/main/java/de/vazzi/ProxyBroadcast.java
+++ ProxyBroadcast/src/main/java/de/vazzi/ProxyBroadcast.java
@@ -1,11 +1,16 @@
 package de.vazzi;
 
 import dev.waterdog.waterdogpe.plugin.Plugin;
+import dev.waterdog.waterdogpe.utils.Configuration;
 
 public class ProxyBroadcast extends Plugin {
 
+    public static Configuration cfg;
+
     @Override
     public void onEnable() {
+        loadConfig();
+        cfg = getConfig();
         getProxy().getCommandMap().registerCommand(new BroadcastCommand());
     }
 
